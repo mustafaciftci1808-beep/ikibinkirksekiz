855075b5b9cc7956afe6e30280d6e88cf4473ed5
mustafaciftci1808-beep <mustafaciftci1808@gmail.com>
2025-11-20 17:18:25 +0300

    Initial Commit

---
+++ ikibinkirksekizTests/ikibinkirksekizTests.swift
@@ -0,0 +1,17 @@
+//
+//  ikibinkirksekizTests.swift
+//  ikibinkirksekizTests
+//
+//  Created by ATS  on 20.11.2025.
+//
+
+import Testing
+@testable import ikibinkirksekiz
+
+struct ikibinkirksekizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
